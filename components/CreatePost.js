9e81e549a494384488fbcb2c599dcea617c3b895
Shabbir Dawoodi <shabbirdwd53@gmail.com>
2022-03-13 13:26:53 +0530

    Create Post

--- components/CreatePost.js
+++ components/CreatePost.js
@@ -1,12 +1,61 @@
 import Image from "next/image";
-import React from "react";
+import { React, useState, useRef } from "react";
 import { HiOutlineVideoCamera } from "react-icons/hi";
 import { IoMdPhotos } from "react-icons/io";
 import { BsEmojiSmile } from "react-icons/bs";
 import { useSession } from "next-auth/react";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import axios from "axios";
 
 const CreatePost = () => {
+  const FACEBOOK_CLONE_ENDPOINT = "http://localhost:8080/api/v1/post";
   const { data: session, status } = useSession();
+  const inputRef = useRef(null);
+  const hiddenFileInput = useRef(null);
+  const [imageToPost, setImageToPost] = useState(null);
+
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputRef.current.value) return;
+    const formData = new FormData();
+
+    formData.append("file", imageToPost);
+    formData.append("post", inputRef.current.value);
+    formData.append("name", session?.user.name);
+    formData.append("email", session?.user.email);
+
+    axios
+      .post(FACEBOOK_CLONE_ENDPOINT, formData, {
+        headers: { Accept: "application/json" },
+      })
+      .then((response) => {
+        inputRef.current.value = "";
+        removeImage();
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = (e) => {
+        setImageToPost(e.target.result);
+      };
+    }
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
+  };
+
   return (
     <div className="bg-white rounded-md shadow-md text-gray-500 p-2 divide-y">
       <div className="flex p-4 space-x-2 items-center">
@@ -21,17 +70,37 @@ const CreatePost = () => {
           <input
             className="rounded-full h-12 flex-grow focus:outline-none font-medium bg-gray-100 px-4"
             type="text"
-            placeholder={`What's on your mind, Shabbir Dawoodi?`}></input>
+            ref={inputRef}
+            placeholder={`What's on your mind, ${session?.user.name}?`}></input>
+          <button hidden onClick={handleSubmit}>
+            Submit
+          </button>
         </form>
       </div>
+      {imageToPost && (
+        <div
+          onClick={removeImage}
+          className="flex items-center px-4 py-2 space-x-4 filter hover:brightness-110 transition duration-150 cursor-pointer">
+          <img src={imageToPost} className="h-10 object-contain" />
+          <RiDeleteBin6Line className="h-8 hover:text-red-500" />
+        </div>
+      )}
       <div className="flex justify-evenly py-2">
         <div className="flex items-center p-1 space-x-1 flex-grow justify-center hover:cursor-pointer hover:bg-gray-100 rounded-md">
           <HiOutlineVideoCamera className="text-red-500" size={20} />
           <p className="font-semibold text-gray-600">Live Video</p>
         </div>
-        <div className="flex items-center p-1 space-x-1 flex-grow justify-center hover:cursor-pointer hover:bg-gray-100 rounded-md">
+        <div
+          onClick={handleClick}
+          className="flex items-center p-1 space-x-1 flex-grow justify-center hover:cursor-pointer hover:bg-gray-100 rounded-md">
           <IoMdPhotos className="text-green-500" size={20} />
           <p className="font-semibold text-gray-600">Photo/Video</p>
+          <input
+            ref={hiddenFileInput}
+            onChange={addImageToPost}
+            type="file"
+            hidden
+          />
         </div>
         <div className="flex items-center p-1 space-x-1 flex-grow justify-center hover:cursor-pointer hover:bg-gray-100 rounded-md">
           <BsEmojiSmile className="text-yellow-400" size={20} />
